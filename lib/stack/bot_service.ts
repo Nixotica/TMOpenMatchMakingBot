6325d12da9930f1752c6f05c18352f826dd5e9d3
Nick Walters <107381993+Nixotica@users.noreply.github.com>
2025-04-24 18:07:36 -0700

    Update bot_service.ts

--- lib/stack/bot_service.ts
+++ lib/stack/bot_service.ts
@@ -197,5 +197,6 @@ export class BotServiceStack extends Stack {
          * Elastic IP
          */
         this.eip = new CfnEIP(this, 'MM-Bot-ElasticIP');
+        // test - trigger redeploy
     }
 }
